406be13d99d691e0aeb8202a10e47c1fe1a50fe3
Laurynas <ltumosa@gmail.com>
2020-10-28 09:34:46 +0200

    Fixed readme, add url mapers

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,7 +1,13 @@
 package app
 
-import "github.com/laur1s/bookstore_users-api/controllers"
+import (
+	"github.com/laur1s/bookstore_users-api/controllers/ping"
+	"github.com/laur1s/bookstore_users-api/controllers/users"
+)
 
 func mapUrls()  {
-	router.GET("/ping",controllers.Ping)
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	//router.GET("/users/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
